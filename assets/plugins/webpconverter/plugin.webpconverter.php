60b25ae30eb5b7b5de4408bd7abf375001d220c4
Martin Plávala <martinplavala@gmail.com>
2021-02-18 22:37:50 +0100

    PHP8 compatibility.

--- assets/plugins/webpconverter/plugin.webpconverter.php
+++ assets/plugins/webpconverter/plugin.webpconverter.php
@@ -12,20 +12,35 @@ function serve_node($node, $atribute) {
 function convert($srcIn) {
     $src = urldecode($srcIn);
 
-    if (in_array(@mime_content_type($src), MIME)) {
+    $srcMime = '';
+    
+    if (file_exists($src)) {
+    	// we set the MIME type as variable to test whether it is supported
+    	// if file_exists evaulates to false, then $srcMime is empty, hence not valid MIME type
+	    $srcMime = mime_content_type($src);
+	}
+
+    if (in_array($srcMime, MIME)) {
+    	// create new file name - change extension
         $srcArray = explode('.', $src);
         $srcArray[count($srcArray) - 1] = 'webp';
         $webpSrc = implode('.', $srcArray);
+    	// create new file name - add .webp folder
+    	// TODO this only works for URLs not begining with /, this has to be fixed!
         $webpSrcArray = explode('/', $webpSrc);
         array_splice($webpSrcArray, 1, 0, '.webp');
         $webpSrc = implode('/', $webpSrcArray);
         if (!file_exists($webpSrc) or (filectime($webpSrc) < filectime($src))) {
             // image does not exist or is outdated
-            if (mime_content_type($src) == 'image/jpeg') {
+            if ($srcMime == 'image/jpeg') {
                 $image =  imagecreatefromjpeg($src);
+                imagepalettetotruecolor($image);
             }
-            if (mime_content_type($src) == 'image/png') {
+            if ($srcMime == 'image/png') {
                 $image =  imagecreatefrompng($src);
+                imagepalettetotruecolor($image);
+				imagealphablending($image, true);
+				imagesavealpha($image, true);
             }
             array_pop($webpSrcArray);
             $webpSrcDir = implode('/', $webpSrcArray);
@@ -33,7 +48,10 @@ function convert($srcIn) {
                 // folder does not exist
                 mkdir($webpSrcDir, 0755, true);
             }
+            // create webp image
             imagewebp($image, $webpSrc);
+            // free up memory
+            imagedestroy($image);
         }
         if (file_exists($webpSrc) && filesize($webpSrc) > 0 ) {
             // make sure the file really exists 
